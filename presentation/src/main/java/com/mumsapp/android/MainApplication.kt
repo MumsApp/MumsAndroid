9b8c2a314a2e8d878ffef04ee331673d34ab906b
Pawel Dedio <paweldedio@gmail.com>
2018-01-24 19:43:19 +0100

    Added crashlytics

--- presentation/src/main/java/com/mumsapp/android/MainApplication.kt
+++ presentation/src/main/java/com/mumsapp/android/MainApplication.kt
@@ -2,17 +2,24 @@ package com.mumsapp.android
 
 import android.content.Context
 import android.support.multidex.MultiDexApplication
+import com.crashlytics.android.Crashlytics
 import com.mumsapp.android.base.BaseActivity
 import com.mumsapp.android.di.components.ActivityComponent
 import com.mumsapp.android.di.components.AppComponent
 import com.mumsapp.android.di.components.DaggerAppComponent
 import com.mumsapp.android.di.modules.ActivityModule
 import com.mumsapp.android.di.modules.AppModule
+import io.fabric.sdk.android.Fabric
 
 class MainApplication : MultiDexApplication() {
 
     val appComponent: AppComponent by lazy {
         generateAppComponent()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        setupFabric()
     }
 
     companion object {
@@ -30,5 +37,11 @@ class MainApplication : MultiDexApplication() {
 
     fun createActivityComponent(activity: BaseActivity): ActivityComponent {
         return appComponent.plus(ActivityModule(activity))
+    }
+
+    private fun setupFabric() {
+        if (BuildConfig.USE_CRASHLYTICS) {
+            Fabric.with(this, Crashlytics())
+        }
     }
 }
