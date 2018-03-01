fd5f4ec39121b83ad49ce51a98beea16b43a4ca2
Pawel Dedio <paweldedio@gmail.com>
2018-03-01 19:16:56 +0100

    Finished view for children selection

--- presentation/src/main/java/com/mumsapp/android/ui/views/ChildrenSelectionWidget.kt
+++ presentation/src/main/java/com/mumsapp/android/ui/views/ChildrenSelectionWidget.kt
@@ -4,10 +4,28 @@ import android.content.Context
 import android.support.v7.widget.CardView
 import android.util.AttributeSet
 import android.view.View
+import android.widget.TextView
+import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener
 import com.mumsapp.android.R
 
 class ChildrenSelectionWidget : CardView {
+
+    @BindView(R.id.children_selection_widget_children_count)
+    lateinit var childrenCountView: TextView
+
+    @BindView(R.id.children_selection_seek_bar)
+    lateinit var seekBar: RangeSeekBar
+
+    @BindView(R.id.children_selection_range_min)
+    lateinit var selectionRangeMinView: TextView
+
+    @BindView(R.id.children_selection_range_max)
+    lateinit var selectionRangeMaxView: TextView
+
+    private var childrenCount = 0
 
     constructor(context: Context) : super(context) {
         setup(context, null)
@@ -24,5 +42,44 @@ class ChildrenSelectionWidget : CardView {
     private fun setup(context: Context, attrs: AttributeSet?) {
         val view = View.inflate(context, R.layout.item_children_selection_widget, this)
         ButterKnife.bind(view)
+        configureViews()
     }
+
+    private fun configureViews() {
+        setChildrenCount(0)
+        seekBar.setOnRangeSeekbarChangeListener({ minValue, maxValue ->
+            selectionRangeMinView.text = minValue.toString()
+            selectionRangeMaxView.text = maxValue.toString()
+        })
+    }
+
+    @OnClick(R.id.children_selection_widget_plus)
+    fun onPlusClick() {
+        childrenCount++
+        setChildrenCount(childrenCount)
+    }
+
+    @OnClick(R.id.children_selection_widget_minus)
+    fun onMinusClick() {
+        if(childrenCount > 0) {
+            childrenCount--
+            setChildrenCount(childrenCount)
+        }
+    }
+
+    fun setChildrenCount(childrenCount: Int) {
+        this.childrenCount = childrenCount
+        childrenCountView.text = childrenCount.toString()
+    }
+
+    fun getChildrenCount() = childrenCount
+
+    fun setRange(min: Int, max: Int) {
+        seekBar.left = min
+        seekBar.right = max
+    }
+
+    fun getStartRange() = seekBar.left
+
+    fun getEndRange() = seekBar.right
 }
---
+++ presentation/src/main/java/com/mumsapp/android/ui/views/RangeSeekBar.kt
@@ -0,0 +1,14 @@
+package com.mumsapp.android.ui.views
+
+import android.content.Context
+import android.util.AttributeSet
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar
+
+class RangeSeekBar : CrystalRangeSeekbar {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}
