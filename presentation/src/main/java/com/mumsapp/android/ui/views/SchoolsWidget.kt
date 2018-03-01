a840a4cc0dfa746291113a052f92b8623897b97b
Pawel Dedio <paweldedio@gmail.com>
2018-03-01 20:23:15 +0100

    View for schools widget

---
+++ presentation/src/main/java/com/mumsapp/android/ui/views/SchoolsWidget.kt
@@ -0,0 +1,28 @@
+package com.mumsapp.android.ui.views
+
+import android.content.Context
+import android.support.v7.widget.CardView
+import android.util.AttributeSet
+import android.view.View
+import butterknife.ButterKnife
+import com.mumsapp.android.R
+
+class SchoolsWidget : CardView {
+
+    constructor(context: Context) : super(context) {
+        setup(context, null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setup(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        setup(context, attrs)
+    }
+
+    private fun setup(context: Context, attrs: AttributeSet?) {
+        val view = View.inflate(context, R.layout.item_schools_widget, this)
+        ButterKnife.bind(view)
+    }
+}
