56ac679dfe6d701723882cfb72b8511d7747da61
Pawel Dedio <paweldedio@gmail.com>
2018-03-21 19:51:51 +0100

    Finished view for my products cell

---
+++ presentation/src/main/java/com/mumsapp/android/ui/views/GridRecyclerView.kt
@@ -0,0 +1,42 @@
+package com.mumsapp.android.ui.views
+
+import android.content.Context
+import android.support.v7.widget.GridLayoutManager
+import android.util.AttributeSet
+import com.mumsapp.android.R
+
+class GridRecyclerView : BaseRecyclerView {
+
+    constructor(context: Context) : super(context) {
+        setup(context, null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setup(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        setup(context, attrs)
+    }
+
+    private fun setup(context: Context, attrs: AttributeSet?) {
+        val space = context.resources.getDimension(R.dimen.padding_12dp).toInt()
+        val divider = SpacesItemDecoration(space)
+        addItemDecoration(divider)
+        setupAttributes(context, attrs)
+    }
+
+    private fun setupAttributes(context: Context, attrs: AttributeSet?) {
+        val array = context.obtainStyledAttributes(attrs, R.styleable.GridRecyclerView)
+
+        val columnsCount = array.getInteger(R.styleable.GridRecyclerView_columnsCount, 2)
+        setColumnsCount(columnsCount)
+
+        array.recycle()
+    }
+
+    fun setColumnsCount(columnsCount: Int) {
+        val manager = GridLayoutManager(context, columnsCount)
+        layoutManager = manager
+    }
+}
