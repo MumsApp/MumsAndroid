196b180e6b06b9088b43e64811dc7e54ed93bf84
Pawel Dedio <paweldedio@gmail.com>
2018-05-30 17:44:55 +0200

    New children selection widget

--- presentation/src/main/java/com/mumsapp/android/ui/widgets/ChildrenSelectionWidget.kt
+++ presentation/src/main/java/com/mumsapp/android/ui/widgets/ChildrenSelectionWidget.kt
@@ -13,20 +13,6 @@ import com.mumsapp.android.ui.views.RangeSeekBar
 
 class ChildrenSelectionWidget : CardView {
 
-    @BindView(R.id.children_selection_widget_children_count)
-    lateinit var childrenCountView: TextView
-
-    @BindView(R.id.children_selection_seek_bar)
-    lateinit var seekBar: RangeSeekBar
-
-    @BindView(R.id.children_selection_range_min)
-    lateinit var selectionRangeMinView: TextView
-
-    @BindView(R.id.children_selection_range_max)
-    lateinit var selectionRangeMaxView: TextView
-
-    private var childrenCount = 0
-
     constructor(context: Context) : super(context) {
         setup(context, null)
     }
@@ -46,40 +32,5 @@ class ChildrenSelectionWidget : CardView {
     }
 
     private fun configureViews() {
-        setChildrenCount(0)
-        seekBar.setOnRangeSeekbarChangeListener({ minValue, maxValue ->
-            selectionRangeMinView.text = minValue.toString()
-            selectionRangeMaxView.text = maxValue.toString()
-        })
     }
-
-    @OnClick(R.id.children_selection_widget_plus)
-    fun onPlusClick() {
-        childrenCount++
-        setChildrenCount(childrenCount)
-    }
-
-    @OnClick(R.id.children_selection_widget_minus)
-    fun onMinusClick() {
-        if(childrenCount > 0) {
-            childrenCount--
-            setChildrenCount(childrenCount)
-        }
-    }
-
-    fun setChildrenCount(childrenCount: Int) {
-        this.childrenCount = childrenCount
-        childrenCountView.text = childrenCount.toString()
-    }
-
-    fun getChildrenCount() = childrenCount
-
-    fun setRange(min: Int, max: Int) {
-        seekBar.left = min
-        seekBar.right = max
-    }
-
-    fun getStartRange() = seekBar.left
-
-    fun getEndRange() = seekBar.right
 }
